2308bcd9960ce27c0f209588dd536c18c70b76ce
Timothy Andrus <timothy.s.andrus@gmail.com>
2016-01-30 13:10:03 -0600

    finally got every thing working

---
+++ Assets/Scripts/NextLevelTracker.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NextLevelInfo
+{
+	public string levelName;
+	public int levelNumber;
+}
+
+public class NextLevelTracker : MonoBehaviour
+{
+	public static NextLevelTracker instance;
+
+	public static NextLevelInfo nextLevelInfo;
+
+	public string nextLevelName;
+	public int nextLevelNumber;
+
+	void Awake()
+	{
+		instance = this;
+		if (!string.IsNullOrEmpty(nextLevelName))
+		{
+			nextLevelInfo = new NextLevelInfo() { levelName = nextLevelName, levelNumber = nextLevelNumber };
+		}
+	}
+
+	void OnDestroy()
+	{
+		instance = null;
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyUp(KeyCode.J))
+		{
+			PlayNextGameLevel();
+		}
+	}
+
+	public void PlayNextGameLevel()
+	{
+		Application.LoadLevel(nextLevelName);
+	}
+}

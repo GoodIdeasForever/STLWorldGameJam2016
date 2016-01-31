8dec94ef79bba598916a92701dbef9aeb9f6241f
Charles H Andrus <charles.h.andrus@gmail.com>
2016-01-31 16:25:55 -0600

    Added Overall Feedback

---
+++ Assets/Scripts/OverallFeedback.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class OverallFeedback : MonoBehaviour {
+
+	public Slider feedbackSlider;
+	public Player player;
+
+	void Update()
+	{
+		int ritualProgress = 0;
+
+		if (Player.Red == player)
+		{
+			ritualProgress = GameState.instance.SFPandasScore;
+		}
+		if (Player.Blue == player)
+		{
+			ritualProgress = GameState.instance.STLLambsScore;
+		}
+		if (ritualProgress >= 0)
+		{
+			feedbackSlider.value = (float)ritualProgress / (float)4;
+		}
+		else
+		{
+			feedbackSlider.value = 0;
+		}
+	}
+}
